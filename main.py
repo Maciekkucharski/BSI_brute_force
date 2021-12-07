3a9f2c7be829758eedc54660dd601d7c1282c788
unknown <m.kucha@wp.pl>
2021-12-08 00:10:58 +0100

    sec commit

--- main.py
+++ main.py
@@ -128,20 +128,19 @@ swear_words = ['chuj', 'chuja', 'chujek', 'chuju', 'chujem', 'chujnia',
 
 BLOCK_SIZE = 32  # Bytes
 message = b"M\xe9\x07M\x0c\x1f3\xa0\x88L\x08\xde\x9c[\xac\x97;\xe3\xac\x02z\xde\xc7'\xcdx+\xfc\x9b\x0fcr \xed\x0fX\xab\xe7\xed\xba}\xefP\x90-\x92\xb9\xe4+\xc5\xc0\xc4\xc1\x01\xd1oZ\x1eD*\xc6\xf6Ze\xe1\xc8i\xec\x94\xa9\xf0~\xa3\xf1\r]6M\x97\xc8\x80\xce\x1a\x0f\xc9Ky\xd0\x07c=YL\xc1\xff\xb99b\x08\xda\xce\x93\x05\xfc\xe3\x8c\x11\xf7w\xce\xec$\xceu\xb8\xbf\xd5xI\x97\xe1\xe4\xfaK\x11\xb2z\xaaP,Q\xe7\xb7<\xef7\xff\x862\xd0dz^\xb5\r\x89\x8a\xd2d\x8d\xc8\xe7q\xe7\x1e89\x9aN9\x11\xdaHX\xb6\xb0\xe2\x8b^Z\xd3\x87P\xe7;\x87\xd1z^\xa3!\x12g*f\xb1Fe\xdd\x1a\xeb\xde\xc3\x8d\xfd\x99\x7f\xb7\xc4\xdd\x94\xf6\xdd\xd71\x9b\x0e\x04:\x18-\xee}\x0f\xa8#\x15ff\x9a\x9d\xfb\x8fl"
-key = b""
 
-def iterate(buff, depth, decrypt, check):
 
+def iterate(buff: str, depth: int, decrypt, check, hook):
     if depth == 0:
-        check(decrypt(buff))
+        check(decrypt(buff), swear_words, hook)
         return
     for c in range(97, 123):
-        print(type(buff), type(bytes(chr(c), 'utf-8')))
         iterate(
-            buff.join(bytes(chr(c), 'utf-8')),
+            buff + chr(c),
             depth - 1,
             decrypt,
-            check
+            check,
+            hook
         )
 
 
@@ -153,18 +152,18 @@ def contains_a_swear_word(msg: str, list_of_swear_words: list, hook):
     return False
 
 
-def decrypt_password(supposed_key: bytes, message_to_decrypt=message):
-    des = DES.new(supposed_key, DES.MODE_ECB)
-    return des.decrypt(message_to_decrypt)
+def decrypt_password(supposed_key: str, message_to_decrypt=message):
+    des = DES.new(supposed_key.encode(), DES.MODE_ECB)
+    return des.decrypt(message_to_decrypt).decode(encoding='UTF-8', errors='ignore')
 
 
 threads = []
 
-for i in range(8, 9):
-    thread = threading.Thread(target=iterate(key, i, decrypt_password, contains_a_swear_word))
-    threads.append(thread)
-    thread.start()
+with open("testFile", 'w') as f:
+    for i in range(8, 9):
+        thread = threading.Thread(target=iterate("", i, decrypt_password, contains_a_swear_word, f))
+        threads.append(thread)
+        thread.start()
 
 for thread in threads:
     thread.join()
-
